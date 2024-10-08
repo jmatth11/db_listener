a588065bd26b771b9523068d069c2bb484e8714f
Joshua Matthews <jmatth93@gmail.com>
2024-10-07 22:49:36 -0500

    added comment for future feature

--- src/db.zig
+++ src/db.zig
@@ -2,6 +2,25 @@ const std = @import("std");
 const pg = @import("pg");
 const args = @import("args.zig");
 
+//
+// Use this to find foreign key constraints for each table
+//
+//SELECT
+//	kcu.column_name,
+//    ccu.table_schema || '.' || ccu.table_name AS f_table_name,
+//    ccu.column_name AS f_column_name
+//FROM information_schema.table_constraints AS tc
+//JOIN information_schema.key_column_usage AS kcu
+//    ON tc.constraint_name = kcu.constraint_name
+//    AND tc.table_schema = kcu.table_schema
+//JOIN information_schema.constraint_column_usage AS ccu
+//    ON ccu.constraint_name = tc.constraint_name
+//WHERE tc.constraint_type = 'FOREIGN KEY'
+//    AND tc.table_schema='<schema_name>'
+//    AND tc.table_name='<table_name>';
+//
+//
+
 const tables_query = "SELECT table_schema || '.' || table_name FROM information_schema.tables WHERE table_type = 'BASE TABLE' AND table_schema NOT IN ('pg_catalog', 'information_schema', 'typeorm');";
 const create_funcs =
     \\create or replace function {s}_fn()returns trigger as $f$
