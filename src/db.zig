4482179f310cffe379fd5d0e4971e35b9bfb4e5c
Joshua Matthews <jmatth93@gmail.com>
2024-10-07 22:49:36 -0500

    added comment

--- src/db.zig
+++ src/db.zig
@@ -19,6 +19,14 @@ const args = @import("args.zig");
 //    AND tc.table_schema='<schema_name>'
 //    AND tc.table_name='<table_name>';
 //
+// Use this to grab primary key info
+//
+//SELECT c.column_name, c.data_type
+//FROM information_schema.table_constraints tc
+//JOIN information_schema.constraint_column_usage AS ccu USING (constraint_schema, constraint_name)
+//JOIN information_schema.columns AS c ON c.table_schema = tc.constraint_schema
+//  AND tc.table_name = c.table_name AND ccu.column_name = c.column_name
+//WHERE constraint_type = 'PRIMARY KEY' and tc.table_name = '<table_name>' and c.table_schema='<schema>';
 //
 
 const tables_query = "SELECT table_schema || '.' || table_name FROM information_schema.tables WHERE table_type = 'BASE TABLE' AND table_schema NOT IN ('pg_catalog', 'information_schema', 'typeorm');";
